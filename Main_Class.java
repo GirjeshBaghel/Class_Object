8aca8d362ff3f2059590bbfee60e8a355e0bd056
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-20 19:06:55 +0530

    Update Main_Class.java

--- Main_Class.java
+++ Main_Class.java
@@ -2,39 +2,53 @@
 import java.util.Scanner;
 class Main_Class
 {
-   int num,digit,num2,n3,count,sum=0;
+   // class level variable
+   int num,r,num2,n3,count,sum=0;
     Scanner sc = new Scanner(System.in);
-    public void sum()
+    
+   // this method is calling from another class
+   public void sum()
     {
+      
+      // taking the digit of no from user
         System.out.print("Enter the digit to sum : ");
     
         num = sc.nextInt();
-        while(num>0)
+      // checking no is greater than zero or not
+       while(num>0)
         {
-            digit = num%10;
-            sum += digit;
+            r = num%10;
+            sum += r;
             num = num/10;
         }
+      // printing the sum of digit
         System.out.println("Sum of Digit is : "+sum);
     }
    
+   // fibonacci series method is calling from other class
     public void fibo()
-    {       
+    {    
+       // taking two values from user
         System.out.print("Please enter the two value for Fibonacci Series : ");
         num = sc.nextInt();
         num2 = sc.nextInt();
         System.out.println(" Your starting value is : "+num+"   Your Ending value is : "+num2);
         System.out.println("Please enter the Counting value : ");
         count = sc.nextInt();
+       
         for(int i=2 ; i<count; ++i)
         {
+           // first and second value sum transfer in another variable
             n3 = num+num2;
             System.out.print(" "+n3);
-            num = num2;
+           
+           // inter changing the values 
+           num = num2;
             num2 = n3;
         }
     }
     
+   // main method
     public static void main(String[] args)
     {
         
