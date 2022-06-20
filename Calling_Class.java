6e0758bf67faa0cdc6633675f48f8240af6254ea
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-20 18:56:53 +0530

    Update Calling_Class.java

--- Calling_Class.java
+++ Calling_Class.java
@@ -1,8 +1,10 @@
 //Write a program by using class and object.First class do the process of sum of digit and fibonacci series.Second class use the method of first class.
 class Calling_Class
 {
+    // main method
     public static void main(String[] args)
     {
+        // main_class object creating
         Main_Class ob = new Main_Class();
         ob.sum();
         ob.fibo();
