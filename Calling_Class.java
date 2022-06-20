0d4f5874bf34685c6704de7f8ec010cf4af0ab48
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-20 13:57:15 +0530

    Update Calling_Class.java

--- Calling_Class.java
+++ Calling_Class.java
@@ -3,8 +3,7 @@ class Calling_Class
     public static void main(String[] args)
     {
         Main_Class ob = new Main_Class();
-        //ob.sum();
-
+        ob.sum();
         ob.fibo();
     }
 
